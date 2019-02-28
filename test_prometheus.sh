4c36ad8e35d24dc7346062fd0e83547f5559f808
Daniel Hiller <daniel.hiller.1972@gmail.com>
2019-02-28 16:07:05 +0100

    Add prometheus URL with predefined graphs.

--- test_prometheus.sh
+++ test_prometheus.sh
@@ -38,6 +38,7 @@ echo "Executing couple of calls to have something to show in prometheus"
 ab -c 3 -n 60 http://localhost:8080/weather-at/0.1,0.2
 
 echo "Prometheus should be reachable under http://localhost:9090"
+echo "To have some graphs visible use this URL: http://localhost:9090/graph?g0.range_input=15m&g0.expr=http_server_requests_seconds%7Bstatus%3D%22200%22%7D&g0.tab=0&g1.range_input=15m&g1.expr=service_weather_at_seconds&g1.tab=0&g2.range_input=15m&g2.expr=rest_response_write_seconds&g2.tab=0&g3.range_input=15m&g3.expr=backendservice_weather_seconds&g3.tab=0"
 
 while true; do
     sleep 5
